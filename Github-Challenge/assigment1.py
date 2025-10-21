9d9910fbca02f5ebe9303c886696d6bd7a6b0b1d
im-amal-raj <amalrajm.dev@gmail.com>
2025-10-21 19:35:41 +0530

    Challenge 1 Added

---
+++ Github-Challenge/assigment1.py
@@ -0,0 +1,22 @@
+# Get input from user
+
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second nunmber: "))
+
+# Calculations
+
+sum = num1 + num2
+sub = num1 - num2
+mul = num1 * num2 
+div = num1 / num2
+
+# Print result
+
+print(f"""
+      
+    Addition: {sum} 
+    Subtraction: {sub} 
+    Multiplication: {mul} 
+    Division: {div}
+
+""")
