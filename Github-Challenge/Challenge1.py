609b7c6e560c86545b7f8ca4e8b67da7986d80e0
im-amal-raj <amalrajm.dev@gmail.com>
2025-10-28 13:57:25 +0530

    Challenge 2 added

--- Github-Challenge/Challenge1.py
+++ Github-Challenge/Challenge1.py
@@ -1,3 +1,36 @@
+# QUESTION
+"""
+Problem:
+Write a program that takes two numbers as input and prints the results of the following operations:
+Addition
+Subtraction
+Multiplication
+Division
+You should calculate and print all results one by one,
+
+Input:
+Two numbers (integers or floats)
+
+Output:
+
+Sum of the two numbers
+Difference (first number minus second number)
+Product of the two numbers
+Division (first number divided by the second number)
+Example:
+Input:
+8
+4
+Output:
+
+Addition: 12
+Subtraction: 4
+Multiplication: 32
+Division: 2.0
+"""
+
+# CODE
+
 # Get input from user
 
 num1 = int(input("Enter the first number: "))
@@ -7,16 +40,18 @@ num2 = int(input("Enter the second nunmber: "))
 
 sum = num1 + num2
 sub = num1 - num2
-mul = num1 * num2 
+mul = num1 * num2
 div = num1 / num2
 
 # Print result
 
-print(f"""
+print(
+    f"""
       
     Addition: {sum} 
     Subtraction: {sub} 
     Multiplication: {mul} 
     Division: {div}
 
-""")
+"""
+)
--- Github-Challenge/challenge2.py
+++ Github-Challenge/challenge2.py
@@ -1,9 +1,29 @@
+# QUESTION
+
+"""Coding Challenge  Question 2
+
+ Problem : Write a Python program to reverse the elements of a list without using the built-in reverse() method or slicing ([::-1]). You must use a loop to solve this problem.
+Input:
+
+A list of integers (for example: [10, 20, 30, 40, 50]).
+
+Output:
+
+A new list with the elements in reverse order (for the above example: [50, 40, 30, 20, 10]).
+"""
+
+
+# CODE
+
 a = [10, 20, 30, 40, 50]
+
 
 def rev(l):
     temp = []
     for i in l:
-        temp.insert(0,i)
-    print(temp)
+        temp.insert(0, i)
+
+    print(f"Original list: {l} \nReversed list {temp}")
+
 
 rev(a)
