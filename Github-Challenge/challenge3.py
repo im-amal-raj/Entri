69b9f3a7f84aaf9fd86775e5282d460146ae36e4
im-amal-raj <amalrajm.dev@gmail.com>
2025-11-06 17:29:57 +0530

    Challenge 3 added

---
+++ Github-Challenge/challenge3.py
@@ -0,0 +1,48 @@
+import random
+
+bot_point = 0
+user_point = 0
+playing = True
+
+
+def game_mechanics():
+    global user_point, bot_point, result
+    item = ["stone", "paper", "scissor"]
+    bot_pick = random.choice(item)
+    print(bot_pick)
+    while True:
+        user_pick = input("enter a choice: ").strip().lower()
+        if user_pick in item:
+            break
+        else :
+            print("Invalid input pick either stone , paper or scissor")
+    if user_pick == bot_pick:
+        result = "draw"
+        user_point += 1
+        bot_point += 1
+    elif (
+        user_pick == "scissor"
+        and bot_pick == "paper"
+        or user_pick == "stone"
+        and bot_pick == "scissor"
+        or user_pick == "paper"
+        and bot_pick == "stone"
+    ):
+        result = "win"
+        user_point += 2
+    else:
+        result = "lose"
+        bot_point += 2
+
+    print(
+        f"you picked {user_pick} the bot picked {bot_pick} you {result} \n \tpoints \nbot : {bot_point} || you : {user_point}"
+    )
+
+
+while playing:
+    game_mechanics()
+    playing = input(
+        "Do you want to continue ? ... \n pick 'y' to continue playing , type anything else to exit: "
+    ).strip().lower()
+    if playing != "y":
+        playing = False
